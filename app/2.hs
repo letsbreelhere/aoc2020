16b49767f0c173198c472c0c4ae6011e9c064e4c
Bree Elle Gardner <letsbreelhere@gmail.com>
2021-01-01 23:58:56 -0800

    Day 2 pt 2

--- app/2.hs
+++ app/2.hs
@@ -8,10 +8,17 @@ import qualified Data.Text as T
 data Entry = Entry { lo :: Int, hi :: Int, c :: Char, s :: String }
   deriving (Show)
 
-isValid :: Entry -> Bool
-isValid Entry { lo, hi, c, s } =
+isValidPt1 :: Entry -> Bool
+isValidPt1 Entry { lo, hi, c, s } =
   let count = length . filter (== c) $ s
    in count >= lo && count <= hi
+
+isValidPt2 :: Entry -> Bool
+isValidPt2 Entry { lo, hi, c, s } =
+  case (s !! (lo-1) == c, s !! (hi-1) == c) of
+    (True, False) -> True
+    (False, True) -> True
+    _ -> False
 
 entry :: Parser Entry
 entry = do
@@ -31,4 +38,6 @@ main = do
   let parsed = map (\(Right e) -> e) inputs
 
   -- Part 1
-  print . length . filter id . map isValid $ parsed
+  print . length . filter id . map isValidPt1 $ parsed
+  -- Part 2
+  print . length . filter id . map isValidPt2 $ parsed
