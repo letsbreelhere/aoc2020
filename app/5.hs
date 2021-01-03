b7f92bae4d4b4435954e324471e6d11ab042daf9
Bree Elle Gardner <letsbreelhere@gmail.com>
2021-01-03 10:10:50 -0800

    Pt 2

--- app/5.hs
+++ app/5.hs
@@ -14,7 +14,13 @@ rowId = foldl' (\acc b -> acc `shiftL` 1 + (if b then 1 else 0)) 0 . bitsOf
 
 main :: IO ()
 main = do
-  input <- lines <$> readFile "inputs/5.txt"
+  input <- map rowId . lines <$> readFile "inputs/5.txt"
+
+  let minId = minimum input
+      maxId = maximum input
 
   -- Part 1
-  print . maximum . map rowId $ input
+  print maxId
+
+  -- Part 2
+  print $ head ([minId..maxId] \\ input)
