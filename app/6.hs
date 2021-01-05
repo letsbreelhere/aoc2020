b93e27e88ca8cf9b7cdfc49c9f11822ba229ce61
Bree Elle Gardner <letsbreelhere@gmail.com>
2021-01-04 22:42:26 -0800

    6 part 2

--- app/6.hs
+++ app/6.hs
@@ -3,7 +3,15 @@ module Main where
 import Data.List.Split (splitOn)
 import qualified Data.List as L
 
+agreeCount :: [String] -> Int
+agreeCount = length . foldr L.intersect ['a'..'z']
+
 main :: IO ()
 main = do
-  input <- map (L.nub . filter (/= '\n')) . splitOn "\n\n" <$> readFile "inputs/6.txt"
-  print . sum . map length $ input
+  input <- splitOn "\n\n" <$> readFile "inputs/6.txt"
+
+  -- Part 1
+  print . sum . map (length . L.nub . filter (/= '\n')) $ input
+
+  -- Part 2
+  print . sum . map (agreeCount . filter (not . null) . splitOn "\n") $ input
