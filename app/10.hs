d7aa1a6a9097d06dd99e18a98c8ef3d5384cb3e0
Bree Elle Gardner <letsbreelhere@gmail.com>
2021-01-07 10:41:14 -0800

    Day 10 part 2

--- app/10.hs
+++ app/10.hs
@@ -1,11 +1,42 @@
 module Main where
 
 import Data.List
+import Control.Monad
 
 count x = length . filter (== x)
 
+diffs xs = zipWith (-) (tail xs) xs
+
+sublists :: [a] -> [[a]]
+sublists [] = [[]]
+sublists (x:xs) = do
+  sl <- sublists xs
+  [x:sl, sl]
+
+arrangements :: [Int] -> [[Int]]
+arrangements xs = do
+  xs' <- sublists xs
+  guard (not $ null xs')
+  guard (head xs' == head xs)
+  guard (last xs' == last xs)
+  guard (maximum (0 : diffs xs') <= 3)
+  pure xs'
+
+contiguousSublists' :: [Int] -> [Int] -> [[Int]]
+contiguousSublists' [] [] = []
+contiguousSublists' [] (y:ys) = contiguousSublists' [y] ys
+contiguousSublists' xs [] = [xs]
+contiguousSublists' xs (y:ys)
+  | y - last xs > 1 = xs : contiguousSublists' [y] ys
+  | otherwise = contiguousSublists' (xs ++ [y]) ys
+
+contiguousSublists :: [Int] -> [[Int]]
+contiguousSublists = contiguousSublists' []
+
 main :: IO ()
 main = do
-  input <- sort . map (read :: String -> Int) . lines <$> readFile "inputs/10.txt"
-  let diffs = zipWith (-) (tail input) input
-  print $ (count 1 diffs + 1) * (count 3 diffs + 1)
+  is <- sort . map (read :: String -> Int) . lines <$> readFile "inputs/10.txt"
+  let input = 0 : is ++ [last is + 3]
+      ds = diffs input
+  print $ count 1 ds * count 3 ds
+  print . product . map (length . arrangements) $ contiguousSublists input
